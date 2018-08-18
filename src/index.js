35120f61dfdf19137de7cb510b307693768273bb
shanemmay <shanemarvinmay@hotmail.com>
2018-08-17 19:56:30 -0500

    Fixed issue with almost no help from the responses I got on stackoverflow

--- src/index.js
+++ src/index.js
@@ -3,21 +3,58 @@ const fs = require('fs');
 
 const app = express();
 
+//to process post parameters
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 app.get('/', (req,res) => 
 {
-    //trying to write some basic content to the page   
-    fs.readFile('test.txt', (err, data) =>
+    //testing to see what files are in the current directory
+    // fs.readdir(__dirname, (err, files) => 
+    // {
+    //     console.log(files);
+    // });
+
+    //trying to write some basic content to the page    
+    fs.readFile(`${__dirname}/journal_form.html`, (err, data) =>
     {
         res.writeHead(200, {'Content-Type': 'text/html'});
-        res.send("<h1>test complete</h1>");
-        res.write(data);
-       
+        //res.writeHead(200, {'Content-Type': 'text/html'});
+        //YOU CAN ONLY USE ONE RES.SEND!!!
+        //res.send(`<h1>test complete</h1> ${data}`);
+        res.write(`<h1>test complete</h1> ${data}`);
+        // console.log("search entered");
+        // console.log(`${__dirname}/test.txt`);
+        // console.log("data");
+        // console.log(data);
+        // console.log("err");
+        // console.log(err);
+        // console.log("__dirname");
+        // console.log(__dirname);
     });
+ 
+    //res.end();
 });
 
-app.post("/post", (req,res) =>
+app.get("/post", (req,res) =>
 {
-    //res.send("post");
+    res.send("post");
+});  
+
+app.post('/post', function (req, res) {
+    //res.send('POST request to the homepage');
+    res.write(`<h1>Post complete</h1> `);
+
+    let vars = req.body;
+    console.log(vars);
+
+    res.write(JSON.stringify(vars));
+
+    //console.log(req);
+    //console.log(res);
 });
 
 app.listen(8080, () => console.log("App launched"));
