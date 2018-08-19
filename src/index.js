3fea2c21ce4cfddd4d4e5af66497dccce6378c34
shanemmay <shanemarvinmay@hotmail.com>
2018-08-19 14:17:49 -0500

    basic post functionality complete

--- src/index.js
+++ src/index.js
@@ -6,71 +6,18 @@ const app = express();
 //to process post parameters
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use(bodyParser.urlencoded(      // to support URL-encoded bodies
+{     
   extended: true
 })); 
 
-//to get to work on heroku
-const PORT = process.env.PORT || 5000
-
-app.get('/', (req,res) => 
-{
-
-    
-    //testing to see what files are in the current directory
-    // fs.readdir(__dirname, (err, files) => 
-    // {
-    //     console.log(files);
-    // });
-
-    //trying to write some basic content to the page    
-    fs.readFile(`${__dirname}/journal_form.html`, (err, data) =>
-    {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        //res.writeHead(200, {'Content-Type': 'text/html'});
-        //YOU CAN ONLY USE ONE RES.SEND!!!
-        //res.send(`<h1>test complete</h1> ${data}`);
-        res.write(`<h1>test complete</h1> ${data}`);
-        // console.log("search entered");
-        // console.log(`${__dirname}/test.txt`);
-        // console.log("data");
-        // console.log(data);
-        // console.log("err");
-        // console.log(err);
-        // console.log("__dirname");
-        // console.log(__dirname);
-    });
- 
-    //res.end();
-});
-
-app.get("/post", (req,res) =>
-{
-    res.send("post");
-});  
-
-app.post('/post', function (req, res) {
-    //res.send('POST request to the homepage');
-    res.write(`<h1>Post complete</h1> `);
-
-    let vars = req.body;
-    console.log(vars);
-
-    res.write(JSON.stringify(vars));
-
-    //console.log(req);
-    //console.log(res);
-});
-
+//DATABASE INFORMATION AND CONNECTION
 /**
- * DATABASE (HEROKU) START
- */
-/**
-Server: sql3.freemysqlhosting.net
-Name: sql3252727
-Username: sql3252727
-Password: PWQRShW1lV
-Port number: 3306
+*Server: sql3.freemysqlhosting.net
+*Name: sql3252727
+*Username: sql3252727
+*Password: PWQRShW1lV
+*Port number: 3306
  */
 const mysql = require('mysql');
 const connection = mysql.createConnection(
@@ -88,17 +35,53 @@ connection.connect( (err) =>
     console.log("Connected to Database");
 } );
 
-let sql = "SELECT * FROM TEST";
+//to get to work on heroku
+const PORT = process.env.PORT || 5000
 
-connection.query(sql, (err, result) => 
-{
-    if (err) throw err;
-    console.log("Success!");
-    result.forEach(row => 
+app.get('/', (req,res) => 
+{  
+    fs.readFile(`${__dirname}/journal_form.html`, (err, data) =>
     {
-        console.log( row );
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(`<h1>test complete</h1> ${data}`);
     });
+
+    let sql = "SELECT * FROM TEST";
+    connection.query(sql, (err, result) =>
+    {
+      if (err) throw err;
+      //console.log("Query Successful!");
+      //console.log(result);
+      res.write(`<h1>TEST table :</h1>`);
+      res.write( JSON.stringify( result ) );
+    });
+}); 
+
+app.post('/post', function (req, res) 
+{
+    res.write(`<h1>Post complete!</h1>`);
+
+    //SAVING POST IN DB
+    let sql = `INSERT INTO TEST 
+    (name, post) 
+    VALUES 
+    ('${req.body.username}', '${req.body.post}');`;
+
+    connection.query(sql, (err, result) =>
+    {
+      if (err) throw err;
+      console.log("Query Successful!");
+      console.log(result);
+    });
+
+
+    res.write(`
+    <p>${JSON.stringify(req.body)}</p>
+    <p>sql query :  ${sql}</p>
+    `);
+
 });
+
 
 /**
  * SQL Statements made:
@@ -106,8 +89,6 @@ connection.query(sql, (err, result) =>
  * INSERT INTO TEST (post) VALUES ('testing to see if i can insert entry into this table : from localhost')
  * SELECT * FROM TEST
  */
- /**
-  * DATABASE (HEROKU) END
-  */
+
 
 app.listen(PORT, () => console.log("App launched"));
